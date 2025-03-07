0d2cdbcb3251c042dfa532fd7bc0e4b19277e6cb
TemaTech <horiuchi.cadd9@gmail.com>
2025-03-07 22:15:40 +0900

    XMLの構文エラーを修正

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>{{ url('/') }}</loc>
@@ -8,7 +8,9 @@
     @foreach($movies as $movie)
     <url>
         <loc>{{ route('movies.show', $movie->movie_id) }}</loc>
+        @if($movie->release_date)
         <lastmod>{{ $movie->release_date->toAtomString() }}</lastmod>
+        @endif
         <changefreq>monthly</changefreq>
         <priority>0.8</priority>
     </url>

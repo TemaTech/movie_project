a23ffc8550b31a6fd80b95fe5abc33e06f89ff30
TemaTech <horiuchi.cadd9@gmail.com>
2025-04-08 18:59:07 +0900

    tapメソッドを削除

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,21 +16,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->tap(function ($app) {
-        // データベース設定を強制的にMySQLに設定
-        $app['config']->set('database.default', 'mysql');
-        $app['config']->set('database.connections.mysql', [
-            'driver' => 'mysql',
-            'host' => '127.0.0.1',
-            'port' => '3306',
-            'database' => 'movie_ranking',
-            'username' => 'movie_user',
-            'password' => 'movie_password_123',
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ]);
-    })
     ->create();

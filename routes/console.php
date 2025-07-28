622f32c46fc261d21107b4b8a8cf24eb9b3402e6
TemaTech <horiuchi.cadd9@gmail.com>
2025-07-28 21:20:40 +0900

    日本とグローバルの映画データを取得するためのスケジュールされたコマンドを追加。日本の映画データは毎日午前2:30、グローバル映画データは午前3:30に実行される。

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// 毎日午前2:30に日本の映画データを取得
+Artisan::command('schedule:japanese-movies', function () {
+    $this->call('movies:fetch-japanese-boxoffice');
+})->purpose('Fetch Japanese box office data')->dailyAt('02:30');
+
+// 毎日午前3:30にグローバル映画データを取得
+Artisan::command('schedule:global-movies', function () {
+    $this->call('movies:fetch-global-boxoffice');
+})->purpose('Fetch global box office data')->dailyAt('03:30');

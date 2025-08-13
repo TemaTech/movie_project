e686d4c0f6b3faeb47c6db7525591c22812a56ff
TemaTech <horiuchi.cadd9@gmail.com>
2025-08-14 03:18:27 +0900

    フッターのスタイルを更新し、パディングとマージンを調整して全体のレイアウトを改善しました。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,8 +88,8 @@
         }
         .footer {
             background-color: white;
-            padding: 2rem 0;
-            margin-top: 3rem;
+            padding: 1.25rem 0;
+            margin-top: 1.75rem;
             border-top: 1px solid #eee;
         }
         .main-content {
